5f3a0bfe520e4b56e6ac36bb564acd46dc920e05
nakrekarpay1245 <rknyprk2001@gmail.com>
2024-08-15 22:32:06 +0300

    feat: ObjectPool reactored

--- MessyMatch3D/Assets/_Game/Scripts/_helpers/ObjectPool.cs
+++ MessyMatch3D/Assets/_Game/Scripts/_helpers/ObjectPool.cs
@@ -1,48 +1,82 @@
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace _Game.Scripts._helpers
 {
+    /// <summary>
+    /// A generic object pool for MonoBehaviour types, which manages the reuse of objects to avoid unnecessary instantiation and destruction.
+    /// </summary>
+    /// <typeparam name="T">The type of MonoBehaviour object to pool.</typeparam>
     public class ObjectPool<T> where T : MonoBehaviour
     {
         private readonly Func<T> _objectFactory;
+        private readonly Queue<T> _objectPool;
         private readonly int _initialSize;
-        private readonly Queue<T> _objects;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObjectPool{T}"/> class.
+        /// </summary>
+        /// <param name="objectFactory">A function that creates a new instance of the object when needed.</param>
+        /// <param name="initialSize">The initial number of objects to be created in the pool.</param>
         public ObjectPool(Func<T> objectFactory, int initialSize)
         {
-            this._objectFactory = objectFactory;
-            this._initialSize = initialSize;
-            this._objects = new Queue<T>(initialSize);
+            _objectFactory = objectFactory ?? throw new ArgumentNullException(nameof(objectFactory));
+            _initialSize = initialSize > 0 ? initialSize : throw new ArgumentOutOfRangeException(nameof(initialSize), "Initial size must be greater than zero.");
+            _objectPool = new Queue<T>(initialSize);
 
             InitializePool();
         }
 
+        /// <summary>
+        /// Initializes the object pool by creating the initial set of objects.
+        /// </summary>
         private void InitializePool()
         {
             for (int i = 0; i < _initialSize; i++)
             {
-                T obj = _objectFactory();
-                _objects.Enqueue(obj);
+                T obj = CreateObject();
+                _objectPool.Enqueue(obj);
             }
         }
 
-        public T GetObjectFromPool()
+        /// <summary>
+        /// Retrieves an object from the pool. If the pool is empty, a new object is created.
+        /// </summary>
+        /// <returns>An object of type <typeparamref name="T"/> from the pool.</returns>
+        public T GetObject()
         {
-            if (_objects.Count == 0)
+            if (_objectPool.Count == 0)
             {
-                T newObj = _objectFactory();
-                return newObj;
+                return CreateObject();
             }
 
-            return _objects.Dequeue();
+            T obj = _objectPool.Dequeue();
+            obj.gameObject.SetActive(true);
+            return obj;
         }
 
-        public void ReturnObjectToPool(T obj)
+        /// <summary>
+        /// Returns an object to the pool, deactivating it in the process.
+        /// </summary>
+        /// <param name="obj">The object to return to the pool.</param>
+        public void ReturnObject(T obj)
         {
+            if (obj == null) throw new ArgumentNullException(nameof(obj));
+
             obj.gameObject.SetActive(false);
-            _objects.Enqueue(obj);
+            _objectPool.Enqueue(obj);
+        }
+
+        /// <summary>
+        /// Creates a new object using the factory method and deactivates it.
+        /// </summary>
+        /// <returns>A new instance of type <typeparamref name="T"/>.</returns>
+        private T CreateObject()
+        {
+            T obj = _objectFactory();
+            obj.gameObject.SetActive(false);
+            return obj;
         }
     }
 }
