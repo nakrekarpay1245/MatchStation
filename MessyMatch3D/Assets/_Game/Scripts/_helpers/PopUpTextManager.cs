d425665b0f9a21eb615c3a10b5a5288ced837217
nakrekarpay1245 <rknyprk2001@gmail.com>
2024-08-15 22:34:01 +0300

    feat: PopUpText system refactored

--- MessyMatch3D/Assets/_Game/Scripts/_helpers/PopUpTextManager.cs
+++ MessyMatch3D/Assets/_Game/Scripts/_helpers/PopUpTextManager.cs
@@ -1,35 +1,45 @@
 using DG.Tweening;
-using System.Collections;
 using UnityEngine;
+
 namespace _Game.Scripts._helpers
 {
+    /// <summary>
+    /// Manages a pool of PopUpText instances and controls their animation and display.
+    /// </summary>
     public class PopUpTextManager : MonoBehaviour
     {
         [Header("Pop Up Text Manager")]
-        [SerializeField]
-        private PopUpText _popUpTextPrefab;
-        [SerializeField]
-        private int _poolSize = 10;
+        [Tooltip("Prefab for the PopUpText.")]
+        [SerializeField] private PopUpText _popUpTextPrefab;
 
-        [SerializeField]
-        private float _positionRandomizer = 0.1f;
+        [Tooltip("Size of the object pool.")]
+        [SerializeField] private int _poolSize = 10;
 
-        [SerializeField]
-        private float _verticalPositionOverride = 0.25f;
+        [Tooltip("Randomizes the position of the pop-up text.")]
+        [SerializeField] private float _positionRandomizer = 0.1f;
 
-        [SerializeField]
-        private Vector3 _popUpTextScale;
+        [Tooltip("Override for the vertical position adjustment of the pop-up text.")]
+        [SerializeField] private float _verticalPositionOverride = 0.25f;
+
+        [Tooltip("Scale applied to the pop-up text during animation.")]
+        [SerializeField] private Vector3 _popUpTextScale = Vector3.one;
 
         private ObjectPool<PopUpText> _popUpTextPool;
 
-        private float PopUpTextAnimationDuration = 0.25f;
-        private float PopUpTextAnimationDelay = 0.125f;
+        // Animation settings
+        private readonly float _popUpTextAnimationDuration = 0.25f;
+        private readonly float _popUpTextAnimationDelay = 0.125f;
 
         private void Awake()
         {
-            _popUpTextPool = new ObjectPool<PopUpText>(() => InstantiatePopUpText(), _poolSize);
+            // Initialize the object pool with the specified size.
+            _popUpTextPool = new ObjectPool<PopUpText>(InstantiatePopUpText, _poolSize);
         }
 
+        /// <summary>
+        /// Instantiates a new PopUpText instance and adds it to the pool.
+        /// </summary>
+        /// <returns>A new PopUpText instance.</returns>
         private PopUpText InstantiatePopUpText()
         {
             PopUpText popUpTextInstance = Instantiate(_popUpTextPrefab, transform);
@@ -37,45 +47,62 @@ namespace _Game.Scripts._helpers
             return popUpTextInstance;
         }
 
-        public void ShowPopUpText(Vector3 position, string text, /*Quaternion rotation,*/  float duration = 0.25f)
+        /// <summary>
+        /// Displays a pop-up text at the specified position with optional custom duration.
+        /// </summary>
+        /// <param name="position">The position where the pop-up text will appear.</param>
+        /// <param name="text">The text to display.</param>
+        /// <param name="duration">The duration before the pop-up text is hidden.</param>
+        public void ShowPopUpText(Vector3 position, string text, float duration = 0.25f)
         {
-            PopUpText popUpText = _popUpTextPool.GetObjectFromPool();
-
-            popUpText.transform.position = position +
-                Vector3.right * Random.Range(-_positionRandomizer, _positionRandomizer) +
-                    Vector3.up * Random.Range(-_positionRandomizer, _positionRandomizer);
-            //popUpText.transform.rotation = rotation;
+            PopUpText popUpText = _popUpTextPool.GetObject();
+            SetPopUpTextPosition(popUpText, position);
             popUpText.SetText(text);
 
-            popUpText.transform.localScale = Vector3.zero;
+            AnimatePopUpText(popUpText, duration);
+        }
 
-            popUpText.gameObject.SetActive(true);
+        /// <summary>
+        /// Sets the position of the pop-up text with a randomized offset.
+        /// </summary>
+        /// <param name="popUpText">The PopUpText instance.</param>
+        /// <param name="position">The target position.</param>
+        private void SetPopUpTextPosition(PopUpText popUpText, Vector3 position)
+        {
+            popUpText.transform.position = position +
+                Vector3.right * Random.Range(-_positionRandomizer, _positionRandomizer) +
+                Vector3.up * Random.Range(-_positionRandomizer, _positionRandomizer);
+        }
 
+        /// <summary>
+        /// Animates the pop-up text and hides it after the specified duration.
+        /// </summary>
+        /// <param name="popUpText">The PopUpText instance.</param>
+        /// <param name="duration">The duration before hiding the pop-up text.</param>
+        private void AnimatePopUpText(PopUpText popUpText, float duration)
+        {
             Vector3 startPosition = popUpText.transform.position;
             Vector3 endPosition = startPosition + Vector3.up * _verticalPositionOverride;
 
             Sequence popUpSequence = DOTween.Sequence();
 
-            popUpSequence.Append(popUpText.transform.DOScale(_popUpTextScale, PopUpTextAnimationDuration).
-                SetEase(Ease.OutBack))
-                         .Join(popUpText.transform.DOMove(endPosition, PopUpTextAnimationDuration).
-                         SetEase(Ease.OutQuad))
-                         .AppendInterval(PopUpTextAnimationDelay)
-                         .Append(popUpText.transform.DOScale(0, PopUpTextAnimationDuration).
-                         SetEase(Ease.InQuad))
-                         .Join(popUpText.transform.DOMove(endPosition + Vector3.up *
-                            _verticalPositionOverride, PopUpTextAnimationDuration).
-                         SetEase(Ease.InQuad))
-                         .OnComplete(() => StartCoroutine(HidePopUpText(popUpText, duration)));
-
-            popUpSequence.Play();
+            popUpSequence.Append(popUpText.transform.DOScale(_popUpTextScale, _popUpTextAnimationDuration).SetEase(Ease.OutBack))
+                         .Join(popUpText.transform.DOMove(endPosition, _popUpTextAnimationDuration).SetEase(Ease.OutQuad))
+                         .AppendInterval(_popUpTextAnimationDelay)
+                         .Append(popUpText.transform.DOScale(Vector3.zero, _popUpTextAnimationDuration).SetEase(Ease.InQuad))
+                         .Join(popUpText.transform.DOMove(endPosition + Vector3.up * _verticalPositionOverride, _popUpTextAnimationDuration).SetEase(Ease.InQuad))
+                         .OnComplete(() => ReturnPopUpTextToPool(popUpText, duration));
         }
 
-        private IEnumerator HidePopUpText(PopUpText popUpText, float duration)
+        /// <summary>
+        /// Returns the pop-up text to the pool after the animation is complete.
+        /// </summary>
+        /// <param name="popUpText">The PopUpText instance.</param>
+        /// <param name="duration">The duration before hiding the pop-up text.</param>
+        private void ReturnPopUpTextToPool(PopUpText popUpText, float duration)
         {
-            yield return new WaitForSeconds(duration);
-
-            _popUpTextPool.ReturnObjectToPool(popUpText);
+            popUpText.gameObject.SetActive(false);
+            _popUpTextPool.ReturnObject(popUpText);
         }
     }
 }
