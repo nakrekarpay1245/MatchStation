828ad6b984fa00bd30c28998040b6f349b81b2d1
nakrekarpay1245 <rknyprk2001@gmail.com>
2024-08-15 22:34:24 +0300

    feat: Item refactored

--- MessyMatch3D/Assets/_Game/Scripts/Item/Item.cs
+++ MessyMatch3D/Assets/_Game/Scripts/Item/Item.cs
@@ -6,41 +6,83 @@ using UnityEngine;
 namespace _Game.Scripts.Items
 {
     /// <summary>
-    /// Represents an item in the game. This class encapsulates the item's ID, icon, and description.
+    /// Represents an item in the game. This class encapsulates the item's properties and behaviors.
     /// </summary>
     public class Item : MonoBehaviour, ISelectable, ICollectable
     {
         [Header("Item Properties")]
         [Tooltip("Unique identifier for the item.")]
-        [SerializeField]
+        [SerializeField, HideInInspector]
         private int _itemId;
-
-        /// <summary>
-        /// Gets the unique identifier for the item.
-        /// </summary>
-        public int ItemId
-        {
-            get => _itemId;
-            private set => _itemId = value; // Can be set only internally.
-        }
 
         [Tooltip("Icon representing the item.")]
         [SerializeField]
         private Sprite _itemIcon;
 
+        [Tooltip("Description of the item.")]
+        [TextArea, SerializeField]
+        private string _itemDescription;
+
+        [Header("Tile Parameters")]
+        [Tooltip("Tile associated with the item.")]
+        [SerializeField]
+        private Tile _itemTile;
+
+        [Header("Item Move Settings")]
+        [Tooltip("Duration for items to move to their new positions.")]
+        [SerializeField]
+        private float _itemMoveDuration = 0.5f;
+
+        [Tooltip("Position offset when the item moves.")]
+        [SerializeField]
+        private Vector3 _itemPositionOffset = Vector3.up;
+
+        [Header("Scale Parameters")]
+        [Tooltip("Rotation of the item when it is collected.")]
+        [SerializeField]
+        private Vector3 _itemCollectRotation = new Vector3(0f, 90f, 90f);
+
+        [Tooltip("Scale multiplier for the item when it is in its normal state.")]
+        [SerializeField]
+        private float _itemNormalScaleMultiplier = 1f;
+
+        [Tooltip("Scale multiplier for the item when it is selected.")]
+        [SerializeField]
+        private float _itemSelectedMultiplier = 1.25f;
+
+        [Tooltip("Scale multiplier for the item when it is collected.")]
+        [SerializeField]
+        private float _itemCollectedScaleMultiplier = 0.5f;
+
+        [Tooltip("Duration of the scale change animation.")]
+        [SerializeField]
+        private float _itemScaleChangeDuration = 0.2f;
+
+        [Header("Effects")]
+        [Tooltip("Particle effect key for when the item is collected.")]
+        [SerializeField]
+        private string _itemCollectParticleKey = "ItemCollect";
+
+        [Tooltip("Audio clip key for when the item is collected.")]
+        [SerializeField]
+        private string _itemCollectClipKey = "ItemCollect";
+
+        private Rigidbody _rigidbody;
+        private bool _isCollectable = true;
+
         /// <summary>
-        /// Gets the icon representing the item.
+        /// Gets the unique identifier for the item.
+        /// </summary>
+        public int ItemId => _itemId;
+
+        /// <summary>
+        /// Gets or sets the icon representing the item.
         /// </summary>
         public Sprite ItemIcon
         {
             get => _itemIcon;
             set => _itemIcon = value;
         }
-
-        [Tooltip("Description of the item.")]
-        [TextArea]
-        [SerializeField]
-        private string _itemDescription;
 
         /// <summary>
         /// Gets or sets the description of the item.
@@ -51,125 +93,113 @@ namespace _Game.Scripts.Items
             set => _itemDescription = value;
         }
 
-        [Header("Tile Parameters")]
-        [Tooltip("")]
-        [SerializeField]
-        private Tile _itemTile;
+        /// <summary>
+        /// Gets or sets the tile associated with the item.
+        /// </summary>
         public Tile ItemTile
         {
-            get
-            {
-                return _itemTile;
-            }
+            get => _itemTile;
             set
             {
                 _itemTile = value;
-                Vector3 itemPosition = _itemTile ? _itemTile.transform.position : Vector3.zero;
-                Vector3 itemRotation = _itemTile ? _itemCollectRotation : Vector3.zero;
-
-                if (IsCollectable)
-                {
-                    GlobalBinder.singleton.ParticleManager.PlayParticleAtPoint(_itemCollectParticleKey,
-                            transform.position);
-
-                    GlobalBinder.singleton.AudioManager.PlaySound(_itemCollectClipKey);
-                }
-
-                transform.DOMove(itemPosition + _itemPositionOffset, _itemMoveDuration);
-
-                transform.DORotate(itemRotation, _itemMoveDuration);
+                UpdateItemPosition();
+                PlayCollectionEffects();
             }
         }
 
-        [Header("Item Move Settings")]
-        [Tooltip("Duration for items to move to their new positions.")]
-        [SerializeField]
-        private float _itemMoveDuration = 0.5f;
-
-        [Tooltip("")]
-        [SerializeField]
-        private Vector3 _itemPositionOffset = Vector3.up;
-
-        [Header("Scale Parameters")]
-        [Tooltip("")]
-        [SerializeField]
-        private Vector3 _itemCollectRotation = new Vector3(0f, 90f, 90f);
-
-        [Header("Scale Parameters")]
-        [Tooltip("")]
-        [SerializeField]
-        private float _itemNormalScaleMultiplier = 1f;
-        public float ItemGenerateScaleMultiplier => _itemNormalScaleMultiplier;
-        [Tooltip("")]
-        [SerializeField]
-        private float _itemSelectedMultiplier = 1.25f;
-        [Tooltip("")]
-        [SerializeField]
-        private float _itemCollectedScaleMultiplier = 0.5f;
-        [Tooltip("")]
-        [SerializeField]
-        private float _itemScaleChangeDuration = 0.2f;
-
-        private bool _isCollectable = true;
-        public bool IsCollectable { get => _isCollectable; private set => _isCollectable = value; }
-
-        [Header("Effects")]
-        [Header("Particle Effects")]
-        [SerializeField, Tooltip("")]
-        private string _itemCollectParticleKey = "ItemCollect";
-        [Header("Audio Effects")]
-        [SerializeField, Tooltip("")]
-        private string _itemCollectClipKey = "ItemCollect";
-
-        private Rigidbody _rigidbody;
+        /// <summary>
+        /// Gets a value indicating whether the item is collectable.
+        /// </summary>
+        public bool IsCollectable => _isCollectable;
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody>();
-            transform.localScale = Vector3.one * _itemNormalScaleMultiplier;
+            ResetItemScale();
         }
 
         /// <summary>
-        /// Handles the selection of the item and applies a pop-up scale animation.
+        /// Handles the selection of the item and applies a scale animation.
         /// </summary>
         public void Select()
         {
-            //Debug.Log(name + "Selected!");
-            // Apply a scale effect using DOTween
-            transform.DOScale(Vector3.one * _itemSelectedMultiplier, _itemScaleChangeDuration); // Scale up 
+            ApplyScaleAnimation(_itemSelectedMultiplier);
         }
 
+        /// <summary>
+        /// Handles the deselection of the item and reverts the scale.
+        /// </summary>
         public void DeSelect()
         {
-            //Debug.Log(name + "DeSelected!");
-            // Apply a scale effect using DOTween
-            transform.DOScale(Vector3.one * _itemNormalScaleMultiplier, _itemScaleChangeDuration); // Scale back to original
+            ApplyScaleAnimation(_itemNormalScaleMultiplier);
         }
 
+        /// <summary>
+        /// Handles the collection of the item.
+        /// </summary>
         public void Collect()
         {
             _isCollectable = false;
-
-            //Debug.Log(name + "Collected!");
-            // Apply a scale effect using DOTween
-            // Scale back to original
-            transform.DOScale(Vector3.one * _itemCollectedScaleMultiplier, _itemScaleChangeDuration).OnComplete(() =>
+            ApplyScaleAnimation(_itemCollectedScaleMultiplier, () =>
             {
                 _rigidbody.isKinematic = true;
             });
         }
 
+        /// <summary>
+        /// Recycles the item, resetting its collectable state and scale.
+        /// </summary>
         public void Recycle()
         {
             _isCollectable = true;
-
-            //Debug.Log(name + " Recycled!");
-            // Apply a scale effect using DOTween
-            // Scale back to original
-            transform.DOScale(Vector3.one * _itemNormalScaleMultiplier, 0.2f).OnComplete(() =>
+            ApplyScaleAnimation(_itemNormalScaleMultiplier, () =>
             {
                 _rigidbody.isKinematic = false;
             });
+        }
+
+        /// <summary>
+        /// Resets the scale of the item to its normal state.
+        /// </summary>
+        private void ResetItemScale()
+        {
+            transform.localScale = Vector3.one * _itemNormalScaleMultiplier;
+        }
+
+        /// <summary>
+        /// Updates the item's position and rotation based on the associated tile.
+        /// </summary>
+        private void UpdateItemPosition()
+        {
+            if (_itemTile == null) return;
+
+            Vector3 itemPosition = _itemTile.transform.position + _itemPositionOffset;
+            Vector3 itemRotation = _itemCollectRotation;
+
+            transform.DOMove(itemPosition, _itemMoveDuration);
+            transform.DORotate(itemRotation, _itemMoveDuration);
+        }
+
+        /// <summary>
+        /// Plays particle and sound effects when the item is collected.
+        /// </summary>
+        private void PlayCollectionEffects()
+        {
+            if (IsCollectable)
+            {
+                GlobalBinder.singleton.ParticleManager.PlayParticleAtPoint(_itemCollectParticleKey, transform.position);
+                GlobalBinder.singleton.AudioManager.PlaySound(_itemCollectClipKey);
+            }
+        }
+
+        /// <summary>
+        /// Applies a scale animation to the item.
+        /// </summary>
+        /// <param name="scaleMultiplier">The target scale multiplier.</param>
+        /// <param name="onComplete">Optional callback to be invoked when the animation is complete.</param>
+        private void ApplyScaleAnimation(float scaleMultiplier, TweenCallback onComplete = null)
+        {
+            transform.DOScale(Vector3.one * scaleMultiplier, _itemScaleChangeDuration).OnComplete(onComplete);
         }
     }
 }
