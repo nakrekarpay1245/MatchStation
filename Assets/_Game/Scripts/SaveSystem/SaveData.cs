a92ce7fc71295b21dfe29929b44cfd3aab90e23a
nakrekarpay1245 <rknyprk2001@gmail.com>
2024-08-25 12:46:10 +0300

    feat: Add Save System

---
+++ Assets/_Game/Scripts/SaveSystem/SaveData.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+/// <summary>
+/// SaveData holds the data that will be saved and loaded by the SaveSystem.
+/// For now, this includes the CurrentLevelIndex, but can be expanded as the game grows.
+/// </summary>
+[Serializable]
+public class SaveData
+{
+    [Header("Game Progress")]
+    [Tooltip("The index of the current level the player is on.")]
+    [SerializeField]
+    public int CurrentLevelIndex = 0;
+
+    // Future data fields can be added here
+}
--- Assets/_Game/Scripts/_Data/GameData.cs
+++ Assets/_Game/Scripts/_Data/GameData.cs
@@ -1,27 +1,46 @@
-using System.Collections;
+using DG.Tweening.Core.Easing;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace _Game.Scripts.Data
 {
     [CreateAssetMenu(fileName = "GameData", menuName = "Data/GameData")]
-
     public class GameData : ScriptableObject
     {
-        [Header("GAME DATA")]
-        [Tooltip("")]
+        [Header("Game Configuration")]
+        [Tooltip("List of levels in the game.")]
         [SerializeField]
         private List<LevelConfig> _levelList;
 
+        [Tooltip("Current level index in the game.")]
+        [HideInInspector] // We are managing this through SaveManager, so it should not be editable in the inspector.
         [SerializeField]
         private int _currentLevelIndex;
+
+        /// <summary>
+        /// Property to get or set the current level index. 
+        /// When setting, it will save the new value using SaveManager.
+        /// When getting, it will load the value from SaveManager.
+        /// </summary>
         public int CurrentLevelIndex
         {
-            get => _currentLevelIndex % _levelList.Count;
-            set => _currentLevelIndex = value;
+            get
+            {
+                // Load the level index from SaveManager
+                _currentLevelIndex = SaveManager.LoadLevelIndex();
+                return _currentLevelIndex % _levelList.Count;
+            }
+            set
+            {
+                // Set the value and save it via SaveManager
+                _currentLevelIndex = value;
+                SaveManager.SaveLevelIndex(_currentLevelIndex);
+            }
         }
 
+        /// <summary>
+        /// Gets the configuration of the current level based on the index.
+        /// </summary>
         public LevelConfig CurrentLevel => _levelList[CurrentLevelIndex];
-
     }
 }
